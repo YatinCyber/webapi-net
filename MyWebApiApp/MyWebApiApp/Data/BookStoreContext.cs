dacd2a10249b63a7fe74cf103af472b1adf00c27
Nguyen Gia Tin <73213395+YatinCyber@users.noreply.github.com>
2023-04-13 16:05:17 +0700

    first commit Initial commit

---
+++ MyWebApiApp/MyWebApiApp/Data/BookStoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyWebApiApp.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> opt) : base(opt)
+        {
+
+        }
+    }
+}
